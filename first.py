4e4c549923b2d8b8e1c2ff48f5fdf777535b6a6f
AVESH PARMAR <126052252+MRAVESHPARMAR@users.noreply.github.com>
2026-08-20 23:09:07 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+a= int(input("Enter a Number : " ))
+result = "Even" if a%2==0 else "Odd"
+print(result)
+
+x,y,z=9,1,7
+print(max(x,y,z))
+
+#Swapping of two numbers
+a,b=10,7
+a,b=b,a
+print(a,b)
+
